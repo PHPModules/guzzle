273813925ae34a8f4871d1421c83ff4015419fea
twn39 <twn39@163.com>
2018-05-25 14:27:13 +0800

    add files

---
+++ src/ConfigProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Component\Guzzle;
+
+
+use GuzzleHttp\Client;
+
+class ConfigProvider
+{
+    public function __invoke() : array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+        ];
+    }
+
+    public function getDependencies() : array
+    {
+        return [
+            'invokables' => [
+                Client::class => Client::class,
+            ],
+        ];
+    }
+}
